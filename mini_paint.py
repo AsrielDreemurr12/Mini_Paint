63474efd8277d633d56cc84df73810c837507f90
Alia Kakabaeva <a.kakabaeva@yandex.ru>
2020-12-30 09:05:16 +0300

    first commit

---
+++ mini_paint.py
@@ -0,0 +1,81 @@
+from tkinter import*
+from random import*
+
+color='black'
+width=2
+clicked=[False,False]
+
+colors=['white','red','orange','mediumvioletred','gold','green','blueviolet','cyan','blue','bisque','black']
+colors_btns=[]
+
+def palitra(event):
+    global color
+    t=event.widget
+    color=t['bg']
+
+def random_color():
+    global color
+    rd_color='#%0x%0x%0x'%(randint(0,15),randint(0,15),randint(0,15))
+    color=rd_color    
+    
+def draw(event):
+    global color, width, c
+    x=event.x
+    y=event.y
+    c.create_oval(x-width,y-width,x+width,y+width,fill=color,outline=color)
+
+def width_config():
+    global entry, width
+    width=int(entry.get())
+
+def clear():
+    global c
+    c.delete('all')
+
+def filled():
+    global c, color
+    c.config(bg=color)
+
+def rubber():
+    global color, c
+    color=c['bg']
+
+a=Tk()
+a.title('Mini Paint')
+a.iconbitmap('images/icon.ico')
+a.resizable(width=False,height=False)
+
+#цвета
+for i in range(len(colors)):
+    b=Button(bg=colors[i],width=1,height=1)
+    b.bind('<Button-1>',palitra)
+    colors_btns.append(b)
+    b.grid(row=0,column=i+1)        
+
+#иконки
+im=PhotoImage(file='images/filled.gif')
+im1=PhotoImage(file='images/rubber.gif')
+
+c=Canvas(a,width=700,height=500,bg='white')
+c.grid(row=1,column=0)
+
+rd_button=Button(text='random color',command=random_color)
+rd_button.grid(row=0,column=18)
+
+rubber_btn=Button(image=im1,command=rubber)
+rubber_btn.grid(row=0,column=19)
+
+submit_btn=Button(text='Submit',command=width_config)
+submit_btn.grid(row=2,column=14)
+
+entry=Entry(bd=3,width=5)
+entry.grid(row=2,column=15)
+
+filled_button=Button(image=im,command=filled)
+filled_button.grid(row=0,column=13)
+
+clear_btn=Button(text='Очистить',command=clear)
+clear_btn.grid(row=2,column=20)
+
+a.bind('<B1-Motion>',draw)
+a.mainloop()
